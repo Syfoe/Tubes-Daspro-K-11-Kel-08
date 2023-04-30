ade796da5f43a904dc9c0a901a031f027c475818
rhleena <118552028+rhleena@users.noreply.github.com>
2023-04-30 16:55:25 +0700

    Update F12.py

--- F12.py
+++ F12.py
@@ -1,13 +1,14 @@
 def ayamberkokok (user, batchbangun):
     if user == "roro jonggrang": 
+        jumlah_candi = batchbangun
         print("Kukuruyuk.. Kukuruyuk..")
         print()
-        if batchbangun == 100: 
-            print("Jumlah Candi: " + str(batchbangun))
+        if jumlah_candi == 100: 
+            print("Jumlah Candi: " + str(jumlah_candi))
             print()
             print("Yah, Bandung Bondowoso memenangkan permainan!")
         else:
-            print("Jumlah Candi: " + str(batchbangun))
+            print("Jumlah Candi: " + str(jumlah_candi))
             print()
             print("Selamat, Roro Jonggrang memenangkan permainan!")
             print()
